807103b0e5525e8182d3a41b07c481cf00f40549
lbfrancisco <lucasbarroso318@gmail.com>
2022-11-06 23:26:12 -0300

    feat(axios): add fetch request with axios

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 const generateAdvice = async () => {
-  await fetch('https://api.adviceslip.com/advice')
-  .then(response => {return response.json()})
-  .then(data => {
-    document.querySelector('.id').innerText = `ADVICE #${data.slip.id}`
-    document.querySelector('.quote').innerText = `❝${data.slip.advice}❞`
+  await axios.get('https://api.adviceslip.com/advice')
+  .then(response => {
+    document.querySelector('.id').innerText = `ADVICE #${response.data.slip.id}`
+    document.querySelector('.quote').innerText = `❝${response.data.slip.advice}❞`
   })
   .catch(err => console.log(err))
 }
